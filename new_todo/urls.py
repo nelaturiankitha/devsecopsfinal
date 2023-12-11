39d30d40fcc7eec614a5ff42ccf19f430ce823c3
EC2 Default User <ec2-user@ip-172-31-0-162.eu-west-1.compute.internal>
2023-12-11 15:22:37 +0000

    changing urls of main proj

--- new_todo/urls.py
+++ new_todo/urls.py
@@ -21,7 +21,7 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('signup/', include('users.urls')),
-    path('signin/', auth_views.LoginView.as_view(template_name='users/signin.html'), name='sign_in'),
+    path('accounts/login/', auth_views.LoginView.as_view(template_name='users/signin.html'), name='sign_in'),
     path('signout/', auth_views.LogoutView.as_view(template_name='users/signout.html'), name='sign_out'),
     path("", include("todoapp.urls"))
 ]
